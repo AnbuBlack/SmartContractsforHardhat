debfbb46414823576725c5ef6f0071e3211e0aec
Lalo <59518982+AnbuBlack@users.noreply.github.com>
2024-02-06 10:43:56 +0900

    Update deploy_NFT.js

--- scripts/deploy_NFT.js
+++ scripts/deploy_NFT.js
@@ -2,7 +2,7 @@ const main = async () => {
     // const nft_token_contract = await hre.ethers.getContractFactory('MyToken');
     const nft_token_contract = await ethers.getContractFactory('MyToken');
     
-    const contract_deployed_ = await nft_token_contract.deploy("0xFE3B557E8Fb62b89F4916B721be55cEb828dBd73");
+    const contract_deployed_ = await nft_token_contract.deploy("METAMASK ACCOUNT ADDRESS");
     
     console.log('Contract deployed to:', await contract_deployed_.getAddress());
     
