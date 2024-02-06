06271ab0d344481ec7e876ee4d15eb3dbff6bc8c
Lalo <59518982+AnbuBlack@users.noreply.github.com>
2024-02-06 11:04:22 +0900

    Update hardhat.config.js

--- hardhat.config.js_files/hardhat.config.js
+++ hardhat.config.js_files/hardhat.config.js
@@ -8,10 +8,10 @@ module.exports = {
             url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
             accounts: [SEPOLIA_PRIVATE_KEY]
           },
-          BesuQBFT: {
-                url: `http://220.149.235.84:8545`,
+          [NAME_OF_BESU_NETWORK_ON_METAMASK]: {
+                url: `JSONRPC-ADDRESS_FOR_BESU_NODE`,
                 chainId: 1337,
-                accounts: [Besu_IBFT2_PK_ACC]
+                accounts: ["PRIVATE_KEY_FROM_METAMASK_ACCOUNT"]
               }
         }
 };
