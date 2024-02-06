208d0cb6deb283b3ad1f707f052cd9da134cfd3c
Lalo <59518982+AnbuBlack@users.noreply.github.com>
2024-02-06 11:15:39 +0900

    Update hardhat.config.js

--- hardhat.config.js_files/hardhat.config.js
+++ hardhat.config.js_files/hardhat.config.js
@@ -5,10 +5,10 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
           sepolia: {
-            url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+            url: `https://sepolia.infura.io/v3/INFURA_API_KEY`,
             accounts: [SEPOLIA_PRIVATE_KEY]
           },
-          [NAME_OF_BESU_NETWORK_ON_METAMASK]: {
+          NAME_OF_BESU_NETWORK_ON_METAMASK: {
                 url: `JSONRPC-ADDRESS_FOR_BESU_NODE`,
                 chainId: 1337,
                 accounts: ["PRIVATE_KEY_FROM_METAMASK_ACCOUNT"]
